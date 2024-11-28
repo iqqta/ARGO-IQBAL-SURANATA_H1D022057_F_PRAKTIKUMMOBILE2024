88ef7b1e0e5cd25bde44bc649bb609e8e0fb7059
iqqtaa <argo.suranata@mhs.unsoed.ac.id>
2024-11-28 22:38:04 +0700

    Pertemuan 11

---
+++ Pertemuan 11/vue-firebase/src/utils/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpH15YH94gQY_Da66E2BbvinG939pJZNU",
+    authDomain: "vue-firebase-dc0fc.firebaseapp.com",
+    projectId: "vue-firebase-dc0fc",
+    storageBucket: "vue-firebase-dc0fc.firebasestorage.app",
+    messagingSenderId: "977228483591",
+    appId: "1:977228483591:web:d2a36e9f2775dc4060eea2"
+  };
+
+  const firebase = initializeApp(firebaseConfig);
+  const auth = getAuth(firebase);
+  const googleProvider = new GoogleAuthProvider();
+  const db = getFirestore(firebase);
+
+  export { auth, googleProvider, db };
